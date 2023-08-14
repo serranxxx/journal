7baa3c5b9c6739ddbe28da7bdb86cef59c85d78f
serranxxx <albserrano8@gmail.com>
2023-08-13 22:47:31 -0600

    details

--- src/layout/CalendarApp.jsx
+++ src/layout/CalendarApp.jsx
@@ -10,7 +10,7 @@ export const CalendarApp = (props) => {
 
     })
 
-    // const [number, setNumber] = useState(Math.floor(Math.random() * 31) + 1)
+    const [number, setNumber] = useState(Math.floor(Math.random() * 31) + 1)
 
     const customHeaderRender = (headerProps) => {
         const { value, type, onChange, onTypeChange } = headerProps;
@@ -64,8 +64,8 @@ export const CalendarApp = (props) => {
 
                     }}>
                     <img src={data} style={{
-                        width: '70%', position: 'absolute',
-                        marginTop: '0vh'
+                        width: '60%', position: 'absolute',
+                        marginTop: '-4vh'
                     }} />
                 </div>
                 <div
@@ -73,7 +73,7 @@ export const CalendarApp = (props) => {
                     onClick={() => props.handleShowText(date.format('YYYY-MM-DD'))}
                     style={{
 
-                        width: '100%', height: '100%', marginTop:'-25px',
+                        width: '100%', height: '100%', marginTop:'-3vh',
                         alignItems: 'center', justifyContent: 'center',
                         backgroundColor: `${props.color.color_1}20`,position: 'absolute',
 
@@ -91,65 +91,65 @@ export const CalendarApp = (props) => {
         );
     };
 
-    // const addEmotions = () => {
-    //     let nuevoElemento
-    //     if (number < 10) {
-    //         nuevoElemento = `2023-08-0${number}`
-    //     } else if (number >= 10) {
-    //         nuevoElemento = `2023-08-${number}`
-    //     }
+    const addEmotions = () => {
+        let nuevoElemento
+        if (number < 10) {
+            nuevoElemento = `2023-08-0${number}`
+        } else if (number >= 10) {
+            nuevoElemento = `2023-08-${number}`
+        }
 
-    //     setEmotion(prevObjeto => ({
-    //         ...prevObjeto,
-    //         [nuevoElemento]: `${selectEmotion(Math.floor(Math.random() * 36) + 1)}`
-    //     }))
-    //     setNumber(Math.floor(Math.random() * 31) + 1)
-    // }
+        setEmotion(prevObjeto => ({
+            ...prevObjeto,
+            [nuevoElemento]: `${selectEmotion(Math.floor(Math.random() * 36) + 1)}`
+        }))
+        setNumber(Math.floor(Math.random() * 31) + 1)
+    }
 
-    // const selectEmotion = (index) => {
-    //     switch (index) {
-    //         case 1: return emotions.e1
-    //         case 2: return emotions.e2
-    //         case 3: return emotions.e3
-    //         case 4: return emotions.e4
-    //         case 5: return emotions.e5
-    //         case 6: return emotions.e6
-    //         case 7: return emotions.e7
-    //         case 8: return emotions.e8
-    //         case 9: return emotions.e9
-    //         case 10: return emotions.e10
-    //         case 11: return emotions.e11
-    //         case 12: return emotions.e12
-    //         case 13: return emotions.e13
-    //         case 14: return emotions.e14
-    //         case 15: return emotions.e15
-    //         case 16: return emotions.e16
-    //         case 17: return emotions.e17
-    //         case 18: return emotions.e18
-    //         case 19: return emotions.e19
-    //         case 20: return emotions.e20
-    //         case 21: return emotions.e21
-    //         case 22: return emotions.e22
-    //         case 23: return emotions.e23
-    //         case 24: return emotions.e24
-    //         case 25: return emotions.e25
-    //         case 26: return emotions.e26
-    //         case 27: return emotions.e27
-    //         case 28: return emotions.e28
-    //         case 29: return emotions.e29
-    //         case 30: return emotions.e30
-    //         case 31: return emotions.e31
-    //         case 32: return emotions.e32
-    //         case 33: return emotions.e33
-    //         case 34: return emotions.e34
-    //         case 35: return emotions.e35
-    //         case 36: return emotions.e36
+    const selectEmotion = (index) => {
+        switch (index) {
+            case 1: return emotions.e1
+            case 2: return emotions.e2
+            case 3: return emotions.e3
+            case 4: return emotions.e4
+            case 5: return emotions.e5
+            case 6: return emotions.e6
+            case 7: return emotions.e7
+            case 8: return emotions.e8
+            case 9: return emotions.e9
+            case 10: return emotions.e10
+            case 11: return emotions.e11
+            case 12: return emotions.e12
+            case 13: return emotions.e13
+            case 14: return emotions.e14
+            case 15: return emotions.e15
+            case 16: return emotions.e16
+            case 17: return emotions.e17
+            case 18: return emotions.e18
+            case 19: return emotions.e19
+            case 20: return emotions.e20
+            case 21: return emotions.e21
+            case 22: return emotions.e22
+            case 23: return emotions.e23
+            case 24: return emotions.e24
+            case 25: return emotions.e25
+            case 26: return emotions.e26
+            case 27: return emotions.e27
+            case 28: return emotions.e28
+            case 29: return emotions.e29
+            case 30: return emotions.e30
+            case 31: return emotions.e31
+            case 32: return emotions.e32
+            case 33: return emotions.e33
+            case 34: return emotions.e34
+            case 35: return emotions.e35
+            case 36: return emotions.e36
 
 
-    //         default:
-    //             break;
-    //     }
-    // }
+            default:
+                break;
+        }
+    }
 
     useEffect(() => {
 
